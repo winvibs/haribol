79e034ec0892ba9de7886c1037af2b4f397f4e1a
winvibs <winvibs@gmail.com>
2018-02-18 10:17:58 +0100

    Create Search Url: ==================
    --activity_main.xml
    >> Removed the default TextView
    >> Changed Layout from 'Constraint' to 'Linear'
    >> Added 1 EditText and 2 TextViews.
    
    -- MainActivity.java
    >> Added global variables 'mSearchBoxEditText', 'mUrlDisplayTextView' and 'mSearchResultsTextView'
    >> Assigned corresponding UI views/elements to 'mSearchBoxEditText', 'mUrlDisplayTextView' and 'mSearchResultsTextView'
    >> Added method 'makeGithubSearchQuery()'
    >> Updated method 'onOptionsItemSelected(MenuItem item)'
    
    -- NetworkUtils.java
    >> Updated variables 'PARAM_QUERY', 'PARAM_SORT' and 'sortBy' to final.

--- app/src/main/java/com/mobinno/haribol/MainActivity.java
+++ app/src/main/java/com/mobinno/haribol/MainActivity.java
@@ -4,33 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-// Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
-// COMPLETED (2) Create a menu xml called 'main.xml' in the res->menu folder
-// COMPLETED (3) Add one menu item to your menu
-// COMPLETED (4) Give the menu item an id of @+id/action_search
-// COMPLETED (5) Set the orderInCategory to 1
-// COMPLETED (6) Show this item if there is room (use app:showAsAction, not android:showAsAction)
-// COMPLETED (7) Set the title to the search string ("Search") from strings.xml
-// Do 2 - 7 in menu.xml ///////////////////////////////////////////////////////////////////////
+import com.mobinno.haribol.utilities.NetworkUtils;
 
-// COMPLETED (8) Override onCreateOptionsMenu
-// COMPLETED (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
-// COMPLETED (10) Return true to display your menu
-
-// COMPLETED (11) Override onOptionsItemSelected
-// COMPLETED (12) Within onOptionsItemSelected, get the ID of the item that was selected
-// COMPLETED (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-// COMPLETED (14) Don't forgot to call .show() on your Toast
-// COMPLETED (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mSearchBoxEditText;
+    private TextView mUrlDisplayTextView;
+    private TextView mSearchResultsTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
     }
 
     @Override
@@ -43,9 +38,28 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemThatWasClicked = item.getItemId();
         if(itemThatWasClicked == R.id.action_search) {
-            Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show();
+
+            // Call makeGithubSearchQuery when the search menu item is clicked
+            makeGithubSearchQuery();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * makeGithubSearchQuery() will perform the following tasks:
+     * 1. Create the URL to query the github server
+     * 2. --- (to be added)
+     */
+    public void makeGithubSearchQuery() {
+
+        // Fetch the text/keyword entered into the EditText field
+        String githubQuery = mSearchBoxEditText.getText().toString();
+
+        // Create the Final URL using the Search parameter i.e 'githubQuery'
+        URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
+
+        // Display the Final URL on TextView
+        mUrlDisplayTextView.setText(githubSearchUrl.toString());
     }
 }
 
