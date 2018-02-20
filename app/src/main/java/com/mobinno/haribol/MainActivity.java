36cf697089d060a29f76d2eb6623ded2578a007a
linvibs <linvibs@gmail.com>
2018-02-20 20:01:26 +0100

    Implemented ProgressBar and UI Messages for Error scenarios: ============================================================= -- strings.xml >> Added a string stating that an error has occurred.
    -- activity_main.xml
    >> Wrapped the ScrollView in a FrameLayout
    >> Added TextView to display an error message and made it a child of the FrameLayout
    >> Added a ProgressBar and made it a child of the FrameLayout
    
    -- MainActivity.java
    >> Created a method called showJsonDataView to show the data and hide the error
    >> Created a method called showErrorMessage to show the error and hide the data
    >> Overridden 'onPreExecute' to set the loading indicator to visible
    >> Added temp delay on Background Thread to show ProgressBar

--- app/src/main/java/com/mobinno/haribol/MainActivity.java
+++ app/src/main/java/com/mobinno/haribol/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView mUrlDisplayTextView;
     private TextView mSearchResultsTextView;
 
+    // COMPLETED (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorMessageTextView;
+
+    // COMPLETED (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
         mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
         mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
+
+        // COMPLETED (13) Get a reference to the error TextView using findViewById
+        mErrorMessageTextView =(TextView) findViewById(R.id.tv_error_message_display);
+
+        // COMPLETED (25) Get a reference to the ProgressBar using findViewById
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
     }
 
     @Override
@@ -52,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
      * 2. Query the github server over HTTP using the Created URL.
      * 3. Display the Query result in the TextView.
      */
-    public void makeGithubSearchQuery() {
+    private void makeGithubSearchQuery() {
 
         // Fetch the text/keyword entered into the EditText field
         String githubQuery = mSearchBoxEditText.getText().toString();
@@ -64,11 +78,30 @@ public class MainActivity extends AppCompatActivity {
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
 
         // Creating a new GithubQueryTask and calling its execute method, passing in the url to query
-       new GithubQueryTask().execute(githubSearchUrl);
+        new GithubQueryTask().execute(githubSearchUrl);
     }
+
+       // COMPLETED (14) Create a method called showJsonDataView to show the data and hide the error
+       private void showJsonDataView() {
+           mErrorMessageTextView.setVisibility(View.INVISIBLE);
+           mSearchResultsTextView.setVisibility(View.VISIBLE);
+       }
+
+        // COMPLETED (15) Create a method called showErrorMessage to show the error and hide the data
+        private void showErrorMessage() {
+            mSearchResultsTextView.setVisibility(View.INVISIBLE);
+            mErrorMessageTextView.setVisibility(View.VISIBLE);
+        }
 
     // Creating a 'GithubQueryTask' class that extends AsyncTask
     public class GithubQueryTask extends AsyncTask<URL, Void, String> {
+
+        // COMPLETED (26) Override onPreExecute to set the loading indicator to visible
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
 
         // Overriding the 'doInBackground' method to Query the github server and return the results.
         @Override
@@ -83,16 +116,33 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            // added temp delay on Background Thread to show ProgressBar
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             return githubSearchResults;
         }
 
         // Overriding 'onPostExecute' to display the results in the TextView
         @Override
         protected void onPostExecute(String githubSearchResults) {
+
+            // COMPLETED (27) As soon as the loading is complete, hide the loading indicator
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+
             if(githubSearchResults != null && !githubSearchResults.equals("")) {
+                // COMPLETED (17) Call showJsonDataView if we have valid, non-null results
+                showJsonDataView();
                 mSearchResultsTextView.setText(githubSearchResults);
+            } else {
+                // COMPLETED (16) Call showErrorMessage if the result is null in onPostExecute
+                showErrorMessage();
             }
+
         }
     }
 }
-
